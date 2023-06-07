bae874f5171a9d40b05fd6b8921bbdea79e102dd
Valerio Billi <valerio.billi18@gmail.com>
2023-06-07 17:38:32 +0200

    struttura form

---
+++ form.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>form</title>
+</head>
+<body>
+    <form action="index.php" method="GET">
+        <div>
+            <input type="text" name="badword" placeholder="Parola da censurare">
+        </div>
+        <div>
+            <textarea name="paragraph" id="paragraph" cols="30" rows="10" placeholder="Inserisci del testo"></textarea>
+        </div>
+        <button type="submit">Invia</button>
+    </form> 
+</body>
+</html>
